f7308dab4432a4240b8df9884fc2e060ea9d633a
hopepengu <hopepengu@gmail.com>
2022-03-20 22:40:21 +0800

    #AVFoundation_Speech Synthesis / hopePengu 1.0

--- speaker_0317/ViewController.swift
+++ speaker_0317/ViewController.swift
@@ -6,14 +6,69 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var engSpeakerTextField: UITextField!
+    
+    
+    @IBOutlet weak var zhtwSpeakerTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func speakerButton(_ sender: Any) {
+        
+        print("test")
+        // Create an utterance.
+        let utterance = AVSpeechUtterance(string: engSpeakerTextField.text!)
 
+        // Configure the utterance.
+        utterance.rate = 0.57
+        utterance.pitchMultiplier = 0.8
+        utterance.postUtteranceDelay = 0.2
+        utterance.volume = 0.8
+
+        // Retrieve the British English voice.
+        let voice = AVSpeechSynthesisVoice(language: "en-GB")
+
+        // Assign the voice to the utterance.
+        utterance.voice = voice
+        
+        // Create a speech synthesizer.
+        let synthesizer = AVSpeechSynthesizer()
+
+        // Tell the synthesizer to speak the utterance.
+        synthesizer.speak(utterance)
+        
+    }
+    
+    @IBAction func speaker_btn2(_ sender: Any) {
+        
+        // Create an utterance.
+        let utterance = AVSpeechUtterance(string: zhtwSpeakerTextField.text!)
+
+        // Configure the utterance.
+//        utterance.rate = 0.57
+//        utterance.pitchMultiplier = 0.8
+//        utterance.postUtteranceDelay = 0.2
+//        utterance.volume = 0.8
+
+        // Retrieve the British English voice.
+        let voice = AVSpeechSynthesisVoice(language: "zh-tw")
+
+        // Assign the voice to the utterance.
+        utterance.voice = voice
+        // Create a speech synthesizer.
+        let synthesizer = AVSpeechSynthesizer()
+
+        // Tell the synthesizer to speak the utterance.
+        synthesizer.speak(utterance)
+    }
+    
 }
+
 
